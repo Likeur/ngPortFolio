a48c6c72fb470f72548f82c475bf76000c9a7712
Likeur <jacksimba28@gmail.com>
2023-11-10 04:06:12 +0200

    gestion des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        title: 'likeur',
+        loadComponent: () => import ('./pages/home-page/home-page.component')
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
