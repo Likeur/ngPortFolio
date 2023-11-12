30d75c06f3caad58ff99f66fa5eb00dccfcaf99f
Likeur <jacksimba28@gmail.com>
2023-11-12 13:05:25 +0200

    overflow s

---
+++ src/app/components/transitionpage/transitionpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-transitionpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transitionpage.component.html',
+  styles: ``
+})
+export class TransitionpageComponent {
+
+}
